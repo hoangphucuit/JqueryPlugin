b198700bdfaa76942d49285cfae089400c989c80
Hoang Phuc <hoangphucuit@gmail.com>
2017-12-29 14:46:43 +0700

    'JqueryPlugin

---
+++ Hamburger-Dropdown-Menu-jQuery/README.md
@@ -0,0 +1,2 @@
+# hamburger-menu-using-jquery
+This is the implementation of hamburger menu for responsive mobile navigation using jquery
---
+++ animate_wow/readme.php
@@ -0,0 +1,18 @@
+<link href="animate_wow/animate.css" type="text/css" rel="stylesheet" />
+<script type='text/javascript' src='animate_wow/wow.min.js'></script>
+<script>
+	new WOW().init();
+</script>
+
+	<div data-wow-delay="0.5s" class="wow rollIn">
+		<img src="http://mynameismatthieu.com/WOW/img/wow-3.gif" height="200">
+	</div>
+	<div data-wow-delay="1s" class="wow bounceInDown center">
+		<img src="http://mynameismatthieu.com/WOW/img/wow-4.gif" height="200">
+	</div>
+	<div data-wow-delay="1.5s" class="wow bounceInRight">
+		<img src="http://mynameismatthieu.com/WOW/img/wow-9.gif" height="200">
+	</div>
+
+
+
